b3afa197b8ab1c890cff95d07d978428240fd6f2
Tanguy Frageul <tanguyfrageul@gmail.com>
2026-07-05 06:38:21 +0200

    🐳 chore(libs): add astro-page-insight

--- astro.config.mjs
+++ astro.config.mjs
@@ -3,7 +3,9 @@ import { defineConfig } from 'astro/config';
 
 import react from '@astrojs/react';
 
+import pageInsight from 'astro-page-insight';
+
 // https://astro.build/config
 export default defineConfig({
-  integrations: [react()]
+  integrations: [react(), pageInsight()]
 });
